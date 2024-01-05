3d2b8d5d145b3c97b5f885e80e1f173ee0c5a542
Kristina Kachalkova <kachalkovakristina@mail.ru>
2024-01-05 16:57:26 +0500

    1.0.1

--- src/components/form-widget/form-widget.js
+++ src/components/form-widget/form-widget.js
@@ -1,39 +1,42 @@
 export default class FormWidget {
-    constructor(element, createImg) {
-        if (typeof element === 'string') {
-            element = document.querySelector(element);
-        }
-        this.element = element;
-        this.createImg = createImg;
-        this.img;
-        this.errorMessage = this.element.querySelector('.message-error');
-
-        this.onClick = this.onClick.bind(this);
-        this.onLoad = this.onLoad.bind(this);
-        this.onError = this.onError.bind(this);
-
-        this.element.addEventListener('submit', this.onClick);
+  constructor(element, createImg) {
+    if (typeof element === 'string') {
+      this.element = document.querySelector(element);
+    } else {
+      this.element = element;
     }
-    onClick(e) {
-        e.preventDefault();
+    this.createImg = createImg;
+    this.img = '';
+    this.errorMessage = this.element.querySelector('.message-error');
 
-        const input = this.element.querySelector('.input-link');
-        const url = input.value;
+    this.onClick = this.onClick.bind(this);
+    this.onLoad = this.onLoad.bind(this);
+    this.onError = this.onError.bind(this);
 
-        this.img = document.createElement('img');
-        this.img.src = url;
+    this.element.addEventListener('submit', this.onClick);
+  }
 
-        this.img.addEventListener('load', this.onLoad);
-        this.img.addEventListener('error', this.onError);
-    }
-    onLoad(e) {
-        this.createImg(this.img);
-        this.errorMessage.classList.add('hidden');
-        this.element.reset();
-    }
-    onError(e) {
-        this.errorMessage.classList.remove('hidden');
-        this.errorMessage.innerText = 'Неверный URL изображения';
-    }
+  onClick(e) {
+    e.preventDefault();
 
+    const input = this.element.querySelector('.input-link');
+    const url = input.value;
+
+    this.img = document.createElement('img');
+    this.img.src = url;
+
+    this.img.addEventListener('load', this.onLoad);
+    this.img.addEventListener('error', this.onError);
+  }
+
+  onLoad() {
+    this.createImg(this.img);
+    this.errorMessage.classList.add('hidden');
+    this.element.reset();
+  }
+
+  onError() {
+    this.errorMessage.classList.remove('hidden');
+    this.errorMessage.innerText = 'Неверный URL изображения';
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
-import ImageList from "../components/image-list/image-list";
-import FormWidget from "../components/form-widget/form-widget";
+import ImageList from '../components/image-list/image-list';
+import FormWidget from '../components/form-widget/form-widget';
 
 const imageList = new ImageList('.image-list');
+
+// eslint-disable-next-line
 const formWidget = new FormWidget('.form', imageList.createImg);
