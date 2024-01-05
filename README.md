a2f1546b8c6c22004ddbf8b496a4f9f6c0108d8c
Kristina Kachalkova <kachalkovakristina@mail.ru>
2024-01-05 17:02:10 +0500

    1.0.3

--- README.md
+++
@@ -1,4 +0,0 @@
-# Image Manager
-
-[![Build status](https://ci.appveyor.com/api/projects/status/3e5soiu13lyrxj5w?svg=true)](https://ci.appveyor.com/project/KristinaKac/events-image-manager)
-
--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import ImageList from '../components/image-list/image-list';
-import FormWidget from '../components/form-widget/form-widget';
-
-const imageList = new ImageList('.image-list');
-
-// eslint-disable-next-line
-const formWidget = new FormWidget('.form', imageList.createImg);
